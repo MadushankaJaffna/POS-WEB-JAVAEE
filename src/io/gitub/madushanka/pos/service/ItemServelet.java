92c97516c969f91039e7a07071536892f99b54c7
madushanka kumara <oska.coc@gmail.com>
2020-01-20 01:13:09 +0530

    Create ItemServelet

--- src/io/gitub/madushanka/pos/service/ItemServelet.java
+++ src/io/gitub/madushanka/pos/service/ItemServelet.java
@@ -14,20 +14,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-@WebServlet(urlPatterns = "/api/v1/customers")
-public class CustomerServelet extends HttpServlet {
+@WebServlet("/api/v1/items")
+public class ItemServelet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Connection connection = DbConnection.getInstance().getConnection();
         try {
-            PreparedStatement prst = connection.prepareStatement("SELECT * FROM Customer");
+            PreparedStatement prst = connection.prepareStatement("SELECT * FROM Item");
             ResultSet resultSet = prst.executeQuery();
             JsonArrayBuilder array = Json.createArrayBuilder();
             while (resultSet.next()) {
                 JsonObjectBuilder obj = Json.createObjectBuilder();
-                obj.add("id", resultSet.getString(1));
-                obj.add("name", resultSet.getString(2));
-                obj.add("address", resultSet.getString(3));
+                obj.add("code", resultSet.getString(1));
+                obj.add("description", resultSet.getString(2));
+                obj.add("qtyOnHand", resultSet.getString(3));
+                obj.add("unitPrice", resultSet.getString(4));
                 array.add(obj);
             }
             resp.setHeader("Access-controll-allow-origin", "*");
@@ -46,10 +47,11 @@ public class CustomerServelet extends HttpServlet {
             JsonReader reader = Json.createReader(req.getReader());
             JsonObject jsonObject = reader.readObject();
 
-            PreparedStatement prstm = connection.prepareStatement("INSERT INTO Customer VALUES(?,?,?)");
-            prstm.setObject(1, jsonObject.getString("id"));
-            prstm.setObject(2, jsonObject.getString("name"));
-            prstm.setObject(3, jsonObject.getString("address"));
+            PreparedStatement prstm = connection.prepareStatement("INSERT INTO Item VALUES(?,?,?,?)");
+            prstm.setObject(1, jsonObject.getString("code"));
+            prstm.setObject(2, jsonObject.getString("description"));
+            prstm.setObject(3, jsonObject.getString("qtyOnHand"));
+            prstm.setObject(3, jsonObject.getString("unitPrice"));
             prstm.executeUpdate();
             connection.close();
         } catch (SQLException e) {
@@ -64,10 +66,11 @@ public class CustomerServelet extends HttpServlet {
             JsonReader reader = Json.createReader(req.getReader());
             JsonObject jsonObject = reader.readObject();
 
-            PreparedStatement prstm = connection.prepareStatement("UPDATE Customer SET name=? , address=? WHERE customerId=? ");
-            prstm.setObject(3, jsonObject.getString("id"));
-            prstm.setObject(1, jsonObject.getString("name"));
-            prstm.setObject(2, jsonObject.getString("address"));
+            PreparedStatement prstm = connection.prepareStatement("UPDATE Item SET description=? ,qtyOnHand=?, unitPrice=? WHERE code=? ");
+            prstm.setObject(4, jsonObject.getString("code"));
+            prstm.setObject(1, jsonObject.getString("description"));
+            prstm.setObject(2, jsonObject.getString("qtyOnHand"));
+            prstm.setObject(3, jsonObject.getString("unitPrice"));
             prstm.executeUpdate();
             connection.close();
         } catch (SQLException e) {
@@ -82,8 +85,8 @@ public class CustomerServelet extends HttpServlet {
             JsonReader reader = Json.createReader(req.getReader());
             JsonObject jsonObject = reader.readObject();
 
-            PreparedStatement prstm = connection.prepareStatement("DELETE FROM Customer WHERE customerId=? ");
-            prstm.setObject(1, jsonObject.getString("id"));
+            PreparedStatement prstm = connection.prepareStatement("DELETE FROM Item  WHERE code=?");
+            prstm.setObject(1, jsonObject.getString("code"));
             prstm.executeUpdate();
             connection.close();
         } catch (SQLException e) {
@@ -91,3 +94,4 @@ public class CustomerServelet extends HttpServlet {
         }
     }
 }
+
