7a9b596d1ef990a5ee8e25a24e667ac6ddc0df3f
madushanka kumara <oska.coc@gmail.com>
2020-01-19 16:47:45 +0530

    Create Customer Servelet

---
+++ src/io/gitub/madushanka/pos/service/CustomerServelet.java
@@ -0,0 +1,9 @@
+package io.gitub.madushanka.pos.service;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/api/v1/customers")
+public class CustomerServelet extends HttpServlet {
+
+}
